bedc8463cbd5097e0a5cb82a84c4bdaddb81b938
yukimemi <yukimemi@gmail.com>
2019-05-05 00:41:51 +0900

    :+1: parse csv log.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 use clap::{
     app_from_crate, crate_authors, crate_description, crate_name, crate_version, AppSettings, Arg,
 };
+use csv;
 use failure::Error;
 use log::*;
 use pretty_env_logger;
+use regex::Regex;
+use settings::{Capture, Settings};
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
+use std::sync::mpsc;
+use std::thread;
+use walkdir::{DirEntry, WalkDir};
 
-use settings::Settings;
 mod settings;
 
-pub type Result<T> = std::result::Result<T, Error>;
+type Result<T> = std::result::Result<T, Error>;
+type Line = HashMap<String, String>;
+
+#[derive(Debug, Clone)]
+struct Rog {
+    name: String,
+    path: PathBuf,
+    capture: Capture,
+    lines: Vec<Line>,
+}
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -21,26 +39,140 @@ fn main() -> Result<()> {
         .arg(Arg::from_usage("<PATH> 'log path'").default_value("."))
         .get_matches();
 
-    debug!("{:?}", &matches);
-
     let cfg = matches.value_of("cfg").unwrap();
-    debug!("{:?}", &cfg);
 
     // Read config.
-    let settings = Settings::new(cfg);
+    let settings = Settings::new(cfg)?;
 
-    debug!("{:?}", &settings);
+    debug!("{:#?}", &settings);
+
+    // Get logs.
+    let input = matches.value_of("PATH").unwrap();
+    let recv = get_entries(input);
+
+    // Filter logs.
+    let rogs = recv
+        .iter()
+        .filter_map(|de| get_rog(de.path(), &settings))
+        .collect::<Vec<_>>();
+
+    // Parse lines of rogs.
+    rogs.iter().map(|rog| rog.parse_lines()).collect::<Vec<_>>();
 
     Ok(())
+}
+
+impl Rog {
+    fn new<P: AsRef<Path>>(name: String, path: P, capture: &Capture) -> Rog {
+        Rog {
+            name: name,
+            path: Path::new(path.as_ref()).to_path_buf(),
+            capture: capture.clone(),
+            lines: vec![],
+        }
+    }
+
+    fn parse_lines(&self) -> Result<Self> {
+        match &self.capture {
+            Capture::Text(cap) => self.parse_with_text(cap.clone()),
+            Capture::Csv(cap) => self.parse_with_csv(cap.clone()),
+        }
+    }
+
+    fn parse_with_csv(&self, cap: HashMap<String, usize>) -> Result<Self> {
+        // Open rog.
+        let f = fs::File::open(&self.path)?;
+        let mut rdr = csv::Reader::from_reader(f);
+        let mut lines: Vec<Line> = Vec::new();
+        rdr.records().for_each(|r| match r {
+            Ok(r) => {
+                // debug!("{:#?}", r);
+                let mut line = HashMap::new();
+                cap.iter().for_each(|(k, v)| {
+                    match r.get(*v) {
+                        Some(col) => {
+                            line.insert(k.to_string(), col.to_string());
+                        }
+                        None => eprintln!("{:#?} has not {} column !", r, v),
+                    };
+                });
+                lines.push(line);
+            }
+            Err(e) => eprintln!("{}", e),
+        });
+
+        // debug!("{:#?}", lines);
+
+        Ok(Rog {
+            lines: lines,
+            ..self.clone()
+        })
+    }
+    fn parse_with_text(&self, cap: String) -> Result<Self> {
+        Ok(Rog { ..self.clone() })
+    }
+}
+
+fn get_rog<P: AsRef<Path>>(path: P, cfg: &Settings) -> Option<Rog> {
+    cfg.rogs.iter().find_map(|rog| {
+        let re = Regex::new(&rog.match_).unwrap();
+        match re.is_match(path.as_ref().to_str().unwrap()) {
+            true => Some(Rog::new(rog.name.to_string(), &path, &rog.capture)),
+            false => None,
+        }
+    })
+}
+
+fn get_entries<P: AsRef<Path>>(path: P) -> mpsc::Receiver<DirEntry> {
+    let (tx, rx) = mpsc::channel();
+    let path = path.as_ref().to_path_buf();
+    thread::spawn(move || {
+        WalkDir::new(path).into_iter().for_each(|e| match e {
+            Ok(e) => {
+                if !e.file_type().is_dir() {
+                    tx.send(e).unwrap()
+                }
+            }
+            Err(e) => eprintln!("{}", e),
+        });
+        drop(tx);
+    });
+    rx
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::env;
+
+    fn get_test_rogs() -> mpsc::Receiver<DirEntry> {
+        let mut cwd = env::current_dir().unwrap();
+        cwd.push("test");
+        get_entries(cwd)
+    }
 
     #[test]
-    fn run_main() {
+    fn test_main() {
         assert_eq!(main().unwrap(), ());
+    }
+
+    #[test]
+    fn test_get_entries() {
+        let rogs = get_test_rogs().iter().map(|e| e).collect::<Vec<_>>();
+        debug!("{:#?}", rogs);
+        assert_eq!(rogs.len(), 3);
+    }
+
+    #[test]
+    fn test_get_rog() {
+        let settings = Settings::new("rog.toml").unwrap();
+        let rogs = get_test_rogs().iter().map(|e| e).collect::<Vec<_>>();
+        let rogs = rogs
+            .iter()
+            .filter_map(|de| get_rog(de.path(), &settings))
+            .collect::<Vec<_>>();
+        debug!("{:#?}", rogs);
+        assert_eq!(rogs.len(), 3);
     }
 
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,26 +1,34 @@
 use config::{Config, ConfigError, Environment, File};
 use serde_derive::Deserialize;
+use std::collections::HashMap;
 
-#[derive(Debug, Deserialize)]
-struct Rog {
-    name: String,
-    extension: String,
-    path: Option<String>,
-    capture: String,
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+pub enum Capture {
+    Text(String),
+    Csv(HashMap<String, usize>),
 }
 
-#[derive(Debug, Deserialize)]
-struct Out {
-    path: String,
-    format: String,
+#[derive(Debug, Clone, Deserialize)]
+pub struct Rog {
+    pub name: String,
+    #[serde(rename = "match")]
+    pub match_: String,
+    pub capture: Capture,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
+pub struct Out {
+    pub path: String,
+    pub format: String,
+    pub fields: Option<Vec<String>>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct Settings {
-    debug: bool,
-    rogs: Vec<Rog>,
-    out: Option<Out>,
-    fields: Option<Vec<String>>
+    pub debug: bool,
+    pub rogs: Vec<Rog>,
+    pub out: Option<Out>,
 }
 
 impl Settings {
@@ -32,15 +40,14 @@ impl Settings {
 
         // Add in a local configuration file
         // This file shouldn't be checked in to git
-        s.merge(File::with_name("config/local").required(false))?;
+        s.merge(File::with_name("local").required(false))?;
 
         // Add in settings from the environment (with a prefix of APP)
         // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         s.merge(Environment::with_prefix("app"))?;
 
         // Now that we're done, let's access our configuration
-        println!("debug: {:?}", s.get_bool("debug"));
-        println!("database: {:?}", s.get::<String>("database.url"));
+        println!("debug: {:#?}", s.get_bool("debug"));
 
         // You can deserialize (and thus freeze) the entire configuration as
         s.try_into()
